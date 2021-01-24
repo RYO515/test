ab5a7abe3e50991cebb215e307791b274837a434
RYO515 <ryota.miyake.515@gmail.com>
2021-01-25 07:33:29 +0900

    fixed and added new files

--- scp_ing_pra/chap4/scp_chap4-19.py
+++ scp_ing_pra/chap4/scp_chap4-19.py
@@ -5,6 +5,8 @@ df = pd.read_csv("firehydrant.csv", encoding="shift-jis")
 
 hydrant = df[["緯度", "経度"]].values
 
+# print(hydrant)
+
 m = folium.Map(location=[35.942957, 136.198863], zoom_start=16)
 for data in hydrant:
     folium.Marker([data[0], data[1]]).add_to(m)
