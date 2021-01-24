5d3635b5d56380fb35a6137718b76d1d7fd18dd5
RYO515 <ryota.miyake.515@gmail.com>
2021-01-24 09:15:55 +0900

    fixed chapter2 directory

---
+++ scp_ing_pra/chap2/scp_chap2-2.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url_load = "https://www.ymori.com/books/python2nen/test1.html"
+html = requests.get(url_load)
+soup = BeautifulSoup(html.content, "html.parser")
+
+print(soup.find("title"))
+print(soup.find("h2"))
+print(soup.find("li"))
